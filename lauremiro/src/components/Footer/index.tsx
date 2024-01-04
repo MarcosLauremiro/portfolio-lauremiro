571c59b826f1c80afc3b31b48ff92634016065ea
MarcosLauremiro <lauremiromelo@gmail.com>
2024-01-04 20:16:33 -0300

    atualizando lins

--- lauremiro/src/components/Footer/index.tsx
+++ lauremiro/src/components/Footer/index.tsx
@@ -7,9 +7,9 @@ export const Footer = () => {
             <div className="footer-container">
                 <div className="links">
                     <a target="blank" href="https://twitter.com/Miroprogramador"><FaXTwitter size='22' /></a>
-                    <a target="blank" href=""><FaInstagram size='22' /></a>
-                    <a target="blank" href=""><FaGithub size='22' /></a>
-                    <a target="blank" href=""><FaYoutube size='22' /></a>
+                    <a target="blank" href="https://www.instagram.com/miroprogramador/?next=%2F"><FaInstagram size='22' /></a>
+                    <a target="blank" href="https://github.com/MarcosLauremiro"><FaGithub size='22' /></a>
+                    <a target="blank" href="https://www.youtube.com/channel/UCbWnBXqSCf-PEvmENWtAvhQ"><FaYoutube size='22' /></a>
                 </div>
                 <div className="brand"><span>Miro ©2023</span></div>
             </div>
--- lauremiro/src/components/Projects/index.tsx
+++ lauremiro/src/components/Projects/index.tsx
@@ -10,68 +10,136 @@ export const Projects = () => {
           envolvido.
         </p>
         <div className="title">
-            <h1>Últimos projetos</h1>
-            <span className="detail">aqui estão alguns dos meus últimos projetos</span>
+          <h1>Últimos projetos</h1>
+          <span className="detail">
+            aqui estão alguns dos meus últimos projetos
+          </span>
         </div>
         <div className="projects">
           <div className="project">
-            <a href="">Uma API com operações CRUD para ferramentas - Back-end.</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/ferramentas"
+            >
+              Uma API com operações CRUD para ferramentas - Back-end.
+            </a>
             <span>19 Dez 2023</span>
           </div>
           <div className="project">
-            <a href="">Uma Agenda de Contatos - Front-end.</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/contatos"
+            >
+              Uma Agenda de Contatos - Front-end.
+            </a>
             <span>15 Dez 2023</span>
           </div>
           <div className="project">
-            <a href="">CRUD Agenda de Contatos - Back-end.</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/api-contatos"
+            >
+              CRUD Agenda de Contatos e usuarios - Back-end.
+            </a>
             <span>14 Dez 2023</span>
           </div>
           <div className="project">
-            <a href="">Link Com butões para todas minhas redes sociais - Front-end.</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/filter_butons"
+            >
+              Link Com butões para todas minhas redes sociais - Front-end.
+            </a>
             <span>12 Dez 2023</span>
           </div>
           <div className="project">
-            <a href="">Loja de roupas FashionStore - Front-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/loja-de-roupas"
+            >
+              Loja de roupas FashionStore - Front-end
+            </a>
             <span>7 Jul 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/hub">Hub de pesquisas de perfis no Github - Front-end</a>
+            <a target="blank" href="https://github.com/MarcosLauremiro/hub">
+              Hub de pesquisas de perfis no Github - Front-end
+            </a>
             <span>30 Jun 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/financeiro-generico">Calculos Financeiro - Front-end</a>
+            <a href="https://github.com/MarcosLauremiro/financeiro-generico">
+              Calculos Financeiro - Front-end
+            </a>
             <span>6 Jun 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/portifolio-generico">Portifolio Basico - Front-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/portifolio-generico"
+            >
+              Portifolio Basico - Front-end
+            </a>
             <span>25 Mai 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/venda-de-lanche">Lanchonete e Pedidos - Front-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/venda-de-lanche"
+            >
+              Lanchonete e Pedidos - Front-end
+            </a>
             <span>16 Jun 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/gestao-de-funcionarios">Gestão interna de funcionarios - Front-end</a>
+            <a href="https://github.com/MarcosLauremiro/gestao-de-funcionarios">
+              Gestão interna de funcionarios - Front-end
+            </a>
             <span>15 Mai 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/clone-rede-social">Clone de rede social - Front-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/clone-rede-social"
+            >
+              Clone de rede social - Front-end
+            </a>
             <span>16 Abr 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/ecommerce-rpg-storage">E-commerce de Rpg - front-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/ecommerce-rpg-storage"
+            >
+              E-commerce de Rpg - front-end
+            </a>
             <span>20 Mar 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/KiMoveis-Imobiliaria">Gerenciamento de serviços par Kimóveis - back-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/KiMoveis-Imobiliaria"
+            >
+              Gerenciamento de serviços par Kimóveis - back-end
+            </a>
             <span>11 Set 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/BandKamp">BandKamp gestão de usuario e musicas - back-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/BandKamp"
+            >
+              BandKamp gestão de usuario e musicas - back-end
+            </a>
             <span>30 Out 2023</span>
           </div>
           <div className="project">
-            <a href="https://github.com/MarcosLauremiro/controle-projetos">Controle de Projetos - back-end</a>
+            <a
+              target="blank"
+              href="https://github.com/MarcosLauremiro/controle-projetos"
+            >
+              Controle de Projetos - back-end
+            </a>
             <span>17 Ago 2023</span>
           </div>
         </div>
